0982035b758311152c8323ff2ea74d4bc7c923ed
Reza <rk.kouhpayeh@gmail.com>
2025-01-07 10:44:12 +0000

    Update src/06_controlflow.js file

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -17,3 +17,64 @@ isJohnEqual = (ageJohn == ageMark);
  console.log("isJohnOlder: " + isJohnOlder);
  console.log("isJohnEqual: " + isJohnEqual);
  console.log("------------------");
+
+ /************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if(true)
+//if(false)
+//if(isJohnOlder)
+//if(ageJohn > ageMark)
+//{
+   // console.log("John ist älter!");
+//}
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+
+
+//if(true)
+//if(false)
+//if(isJohnOlder)
+if(ageJohn > ageMark)
+
+{
+    //console.log("John is älter!");
+
+}
+
+// alternative IF-Zweig 1...n
+else if(isJohnEqual){
+    console.log("John ist gleich alt!")
+}
+
+else
+{
+    console.log("John is jünger!");
+
+}
+
+/************  Ternäre (ternary) Schreibweise ************/
+//console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+
+let tag = 1;
+switch (tag) {
+    case 1:
+      console.log("Montag")
+      break;
+    case 2:
+      console.log("Dinstag")
+      break;
+    default:
+      console.log("Ungültige Tag")
+      break;
+      console.log(1)
+
+  }
+  
+
