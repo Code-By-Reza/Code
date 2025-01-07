6a9873130e26a8bc0f9e2d77545a1e531a1e6a32
Reza <rk.kouhpayeh@gmail.com>
2025-01-07 08:31:28 +0000

    Update src/wahrheitswerte.js file

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -1,17 +1,27 @@
 /* Berechnung Alter */
 
 // Deklaration
-let ageJohn, ageMark;
+let ageReza, ageMark; // ageMark wird deklariert
 const birthYearReza = 1981;
 const birthYearMark = 1990;
 
 // Berechnung Alter
-
-let year  = 2025;
+let date = new Date();
+let year = date.getFullYear(); // "data" wurde zu "date" korrigiert
+// let year = 2025; // Alternativ kann das Jahr manuell angegeben werden
 ageReza = year - birthYearReza;
-agemark = 1981;
-
+ageMark = year - birthYearMark; // Alter von Mark korrekt berechnet
 
 // Ausgabe
 console.log("ageReza: " + ageReza);
-console.log("ageMark: " + ageMark)
+console.log("ageMark: " + ageMark);
+// console.log("Datm. " + date); // Rechtschreibfehler in Kommentar: "Datm" könnte "Datum" sein
+
+// Tests
+
+let isRezaOlder;
+isRezaOlder = (ageReza > ageMark); // Vergleich zwischen ageReza und ageMark
+let isRezaEqual = (ageReza == ageMark); // Variable "isRezaEqual" deklariert
+
+console.log("Is Reza older: " + isRezaOlder);
+console.log("Is Reza equal: " + isRezaEqual); // Ausgabe korrigiert, richtige Variable genutzt
