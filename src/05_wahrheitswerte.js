fc05074c28005902d5438844562c68da867d42cb
Reza <rk.kouhpayeh@gmail.com>
2025-01-07 08:12:15 +0000

    Added src/wahrheitswerte.js file

---
+++ src/05_wahrheitswerte.js
@@ -0,0 +1,17 @@
+/* Berechnung Alter */
+
+// Deklaration
+let ageJohn, ageMark;
+const birthYearReza = 1981;
+const birthYearMark = 1990;
+
+// Berechnung Alter
+
+let year  = 2025;
+ageReza = year - birthYearReza;
+agemark = 1981;
+
+
+// Ausgabe
+console.log("ageReza: " + ageReza);
+console.log("ageMark: " + ageMark)
