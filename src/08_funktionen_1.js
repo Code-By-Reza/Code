322e242c196185d7b698418881267a413bb3c196
Reza <rk.kouhpayeh@gmail.com>
2025-01-08 10:10:29 +0000

    Update: src/08_funktionen_1.js file

--- src/08_funktionen_1.js
+++ src/08_funktionen_1.js
@@ -40,24 +40,42 @@ test();
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-ausgabeNamen_3("Reza");
-function ausgabeNamen_3(firstName) { // Parameter
+//ausgabeNamen_3("Reza");
+//function ausgabeNamen_3(firstName) { // Parameter
 
-    console.log("Hallo, " + firstName +  "\n");
+  //  console.log("Hallo, " + firstName +  "\n");
     
-}
+//}
 
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter / Argumente
-ausgabeNameParamas("Reza", "Kouhpayeh\n");
+//ausgabeNameParamas("Reza", "Kouhpayeh\n");
 
-const prompt = require('prompt-sync')({sigint:true});
-ausgabeNameParamas(prompt("Vorname? :"), prompt("Name? :")); // piping
-
-
+//const prompt = require('prompt-sync')({sigint:true});
+//ausgabeNameParamas(prompt("Vorname? :"), prompt("Name? :")); // piping
 
 
-function ausgabeNameParamas(firstName,secondName) {
-    console.log("Hallo 🙌, " + firstName + " " + secondName );
+
+
+//function ausgabeNameParamas(firstName,secondName) {
+   // console.log("Hallo 🙌, " + firstName + " " + secondName );
+    
+//}
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+ausgabeNamenSRP("Reza", "Kouhpayeh\n");
+
+function ausgabeNamenSRP(firstName,secondName) {
+
+    // 1. Funktionalität: string composing
+
+     const GAP = " ";
+     let outputStr = "Hallo,😊 " + GAP + firstName + GAP + secondName + "!";
+
+    console.log( outputStr);
     
 }
