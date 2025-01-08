1c560996b92f097a60d6fa5bf86be8208caf81bb
Reza <rk.kouhpayeh@gmail.com>
2025-01-08 09:17:33 +0000

    Update: src/08_funktionen_1.js file

--- src/08_funktionen_1.js
+++ src/08_funktionen_1.js
@@ -10,7 +10,39 @@
 
 function test(){
 
-console.log("Hello, World!✌");
+console.log("Hello, World!✌\n");
 
 }
 test();
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+//ausgabeNamen_1();
+//function ausgabeNamen_1() {
+
+    //let firstName = "Reza";
+    //console.log("Hallo, " + firstName + "!");
+    
+//}
+
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+//function ausgabeNamen_2(name) {
+    
+    //console.log(`Hallo ${name}, Wilkommen!`);
+//}
+
+//ausgabeNamen_2("Reza");
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamen_3("Reza");
+function ausgabeNamen_3(firstName) { // Parameter
+
+    console.log("Hallo, " + firstName + "!");
+    
+}
